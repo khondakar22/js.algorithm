85d33215f99746edfdf57c74f74df6849a48bffe
Khondakar Readul Islam <khondakar22readul@gmail.com>
2021-03-26 22:07:33 +0100

    ReverseStringFinish

--- 01.reverse.string/index.js
+++ 01.reverse.string/index.js
@@ -1,11 +1,29 @@
+// -- Directions
+
+// 
+
 function reverse(str) {
-  let reversed = '';
+
+  // Needs some javascript knowledge
+
+  // First solution
+  // return str.split('').reverse().join('')
+  // // Second Solution
+  // let reversed = '';
     
-  for (let char of str) {
-    reversed = char + reversed;
-  }
-  return reversed;
+  // for (let char of str) {
+  //   reversed = char + reversed;
+  // }
+  // return reversed;
+
+  // Third Solution
+  return str.split('').reduce((rev, char) => char + rev , '' ) 
 
 }
+
+reverse('asdf1');
+
+
+
 
 module.exports = reverse;
